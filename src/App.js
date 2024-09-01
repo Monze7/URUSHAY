2e21635ef58f088f5ccd83024591fd305e4cd0b3
Deepshikha <158303301+Deepshikha2529@users.noreply.github.com>
2024-09-01 22:37:47 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
